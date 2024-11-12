71e703488d49d10227f5eb0e62a8c302c59298d3
arvid-persson1 <a.persson04@gmail.com>
2024-11-12 10:21:19 +0100

    part 1

---
+++ report/MCS.typ
@@ -0,0 +1,44 @@
+#set heading(numbering: "1.")
+
+= Specification
+
+Given $A$ of $n = 2^k$ (for some $k in ZZ^+$) non-zero real numbers, determine
+a maximal value $M$ such that $M$ is the sum of some subsequence of $A$.
+
+Note: The assignment does not specify whether to consider 0, the sum of an empty
+subsequence, to be a valid value for $M$, which would be the case if $A$ consists
+solely of negative values. The choice has been made to exclude this possibility.
+
+= Overview
+
+Given the input $A = [a_0, a_1, dots.h a_(n-1)]$, do the following:
+
+- Recursively find $M$ for the two halves of $A$. For a 1-length split $A = [a]$,
+  the only possibility is $M(A) = a$.
+
+- Combine the splits $(L, R)$ pairwise. $M$ is the largest of the following:
+
+    - $M(L)$, i.e. the maximum value of the left split.
+
+    - $M(R)$, i.e. the maximum value of the left split.
+
+    - $M(A_m)$ for any subsequence $A_m$ that crosses the midpoint.
+
+= Implementation
+
+For each recursive step, given $A$, determine a 4-tuple $T = (M, p, s, t)$ where
+$p = M(A_l)$ for any subsequence $A_l$ that includes the first element (the prefix sum),
+$s$ is defined analogously for the last element (the suffix sum),
+and $t$ is the total sum of $A$.
+
+For $A = [a]$, $T$ is trivially $(a, a, a, a)$.
+In other cases, we split $A$ into $(L, R)$ and determine the values of $T$ as follows:
+
+$
+M(A) &= "max"(M(L), M(R), s(L) + p(R)) "," \
+p(A) &= "max"(p(L), t(L) + p(R)) "," \
+s(A) &= "max"(s(R), t(R) + s(L)) "," \
+t(A) &= t(L) + t(R).
+$
+
+The result is $M(T)$.
---
+++ src/mcs.py
@@ -0,0 +1,85 @@
+from dataclasses import dataclass
+from math import inf, isclose
+from random import uniform
+from sys import stderr
+
+K = 5
+MIN = -10
+MAX = 10
+ITERATIONS = 10000
+
+
+# Well-known algorithm to test correctness.
+def maxSubKadane(arr: list[float]) -> float:
+    res = -inf
+    curr = 0
+
+    for x in arr:
+        curr = max(x, curr + x)
+        res = max(res, curr)
+
+    return res
+
+
+@dataclass
+class MaxSubResult:
+    max: float
+    prefix: float
+    suffix: float
+    total: float
+
+
+# The algorithm could be reduced to a single function definition
+# by calling the helper function directly, but this would expose
+# the ugly implementation details (`MaxSubResult`).
+def maxSubRec(arr: list[float]) -> float:
+    assert all(x != 0 for x in arr)
+
+    return _maxSubRec(arr).max
+
+
+def _maxSubRec(arr: list[float]) -> MaxSubResult:
+    n = len(arr)
+
+    assert n > 0
+
+    if n == 1:
+        res = arr[0]
+        return MaxSubResult(res, res, res, res)
+
+    # This verifies that n is a power of 2.
+    assert n & (n - 1) == 0
+
+    mid = n // 2
+    leftRes = _maxSubRec(arr[:mid])
+    rightRes = _maxSubRec(arr[mid:])
+
+    maxSum = max(
+        leftRes.max,
+        rightRes.max,
+        leftRes.suffix + rightRes.prefix,
+    )
+    prefix = max(leftRes.prefix, leftRes.total + rightRes.prefix)
+    suffix = max(rightRes.suffix, rightRes.total + leftRes.suffix)
+    total = leftRes.total + rightRes.total
+
+    return MaxSubResult(maxSum, prefix, suffix, total)
+
+
+def main():
+    for _ in range(ITERATIONS):
+        # We expect this to very rarely create the number 0,
+        # and choose not to correct when it does.
+        arr = [uniform(MIN, MAX) for _ in range(2**K)]
+
+        ans = maxSubKadane(arr)
+        res = maxSubRec(arr)
+        if not isclose(ans, res):
+            print("Recursive failed!", file=stderr)
+            return
+
+    print("Finished successfully.")
+
+
+if __name__ == "__main__":
+    main()
